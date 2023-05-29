c54ec212b312614e6dc1b8d25cc68f8f0846f2bf
mxc <maxiaochong@bilibiili.com>
2023-05-29 20:11:09 +0800

    feat:抽象代码

--- lib/widget/webview_cache_core.dart
+++ lib/widget/webview_cache_core.dart
@@ -6,16 +6,18 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 import '../global.dart';
 
+const _kMaxTimeout = Duration(seconds: 30);
+
 class CacheCoreList extends StatefulWidget {
   final Widget child;
 
   const CacheCoreList({Key? key, required this.child}) : super(key: key);
 
-  static CacheCoreListState? of(BuildContext context) {
+  static CacheCoreListDelegate? of(BuildContext context) {
     return context.findAncestorStateOfType<CacheCoreListState>();
   }
 
-  static CacheCoreListState? ofRoot(BuildContext context) {
+  static CacheCoreListDelegate? ofRoot(BuildContext context) {
     return context.findRootAncestorStateOfType<CacheCoreListState>();
   }
 
@@ -23,74 +25,115 @@ class CacheCoreList extends StatefulWidget {
   State<CacheCoreList> createState() => CacheCoreListState();
 }
 
-class CacheCoreListState extends State<CacheCoreList> {
+class CacheCoreListState extends State<CacheCoreList>
+    implements CacheCoreListDelegate {
+  final List<WebViewCacheCore> cores = [];
+
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: [],
+      children: [
+        ...cores,
+        widget.child,
+      ],
     );
   }
-}
-
-class WebViewCacheCore extends StatefulWidget {
-  final String url;
-
-  const WebViewCacheCore({Key? key, required this.url}) : super(key: key);
 
   @override
-  State<WebViewCacheCore> createState() => _WebViewCacheCoreState();
-
-  static CacheController? of(BuildContext context) {
-    return context
-        .findAncestorStateOfType<_WebViewCacheCoreState>()
-        ?.cacheController;
+  void dispose() {
+    cores.clear();
+    super.dispose();
   }
 
-  static CacheController? ofRoot(BuildContext context) {
+  @override
+  Future<void> addSingleCacheCore(String url,
+      {Function? onStart,
+      Function(int progress)? onProgress,
+      Function? onLoaded,
+      Function? onFailed}) async {}
+}
+
+abstract class CacheCoreListDelegate {
+  Future<void> addSingleCacheCore(String url,
+      {Function? onStart,
+      Function(int progress)? onProgress,
+      Function? onLoaded,
+      Function? onFailed});
+}
+
+abstract class WebViewCacheCore extends StatefulWidget {
+  final String url;
+  final Function? onStart;
+  final Function? onProgress;
+  final Function? onLoaded;
+  final Function? onFailed;
+
+  const WebViewCacheCore(
+      {Key? key,
+      required this.url,
+      this.onStart,
+      this.onProgress,
+      this.onLoaded,
+      this.onFailed})
+      : super(key: key);
+
+  static T? of<T extends CacheController>(BuildContext context) {
     return context
-        .findRootAncestorStateOfType<_WebViewCacheCoreState>()
-        ?.cacheController;
+        .findAncestorStateOfType<_WebViewCacheCoreState>()
+        ?.cacheController as T?;
   }
 }
 
-class _WebViewCacheCoreState extends State<WebViewCacheCore> {
+abstract class _WebViewCacheCoreState extends State<WebViewCacheCore> {
   CacheController? cacheController;
+  bool isPageFinishCalled = false;
 
   @override
   void initState() {
     super.initState();
     _initController().then((value) {
       cacheController = value;
-      setState(() {
-
-      });
+      setState(() {});
     }).catchError((err, stack) {
       debugPrint(stack);
     });
   }
 
+  CacheController provideCacheController(WebViewController controller);
+
   Future<CacheController> _initController() async {
     WebViewController _controller = WebViewController();
+    CacheController cacheController = provideCacheController(_controller);
     await _controller.setJavaScriptMode(JavaScriptMode.unrestricted);
-    await _controller.setBackgroundColor(const Color(0x00000000))
+    await _controller.setBackgroundColor(const Color(0x00000000));
+    await cacheController.injectCore();
     await _controller.setNavigationDelegate(
       NavigationDelegate(
-        onProgress: (int progress) {},
+        onProgress: (int progress) {
+          widget.onProgress?.call(progress);
+        },
         onUrlChange: (UrlChange urlChange) {},
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
+        onPageStarted: (String url) {
+          widget.onStart?.call();
+        },
+        onPageFinished: (String url) {
+          if (!isPageFinishCalled) {
+            isPageFinishCalled = true;
+          }
+          widget.onLoaded?.call();
+        },
         onWebResourceError: (WebResourceError error) {},
         onNavigationRequest: (NavigationRequest request) {
-          if (request.url.startsWith('https://www.youtube.com/')) {
-            return NavigationDecision.prevent;
-          }
-          return NavigationDecision.navigate;
+          return NavigationDecision.prevent;
         },
       ),
     );
-    await _controller.runJavaScript('');
     await _controller.loadRequest(Uri.parse(widget.url));
-    CacheController cacheController = CacheController(_controller);
+    //超时 取消
+    Future.delayed(_kMaxTimeout, () {
+      if (!mounted || isPageFinishCalled) return;
+      widget.onFailed?.call();
+    });
     return cacheController;
   }
 
@@ -108,11 +151,11 @@ class _WebViewCacheCoreState extends State<WebViewCacheCore> {
   }
 }
 
-class CacheController {
+abstract class CacheController {
   final WebViewController webViewController;
 
   CacheController(this.webViewController);
 
-//todo 注入js
+  //todo 注入js
+  Future<void> injectCore();
 }
-
